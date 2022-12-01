51756168aa70e165123a303678da1845e0a8fe45
Marek Pšenka <psenka@edhouse.cz>
2022-12-01 17:16:20 +0100

    Day01

--- src/bin/day01a.rs
+++ src/bin/day01a.rs
@@ -1,5 +1,24 @@
+use itertools::Itertools;
+
 fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
-    }
+    let maybe_lines: Result<Vec<String>, _> = std::io::stdin().lines().collect();
+    let lines = maybe_lines.unwrap();
+
+    let max = lines.into_iter()
+                   .group_by(|l| l.is_empty())
+                   .into_iter()
+                   .filter_map(
+                   |(empty, g)| {
+                       if empty {
+                           None
+                       }
+                       else
+                       {
+                           let items: Result<Vec<i32>, _> = g.map(|l| l.trim().parse::<i32>())
+                                                               .collect();
+                           Some(items.unwrap().iter().sum::<i32>())
+                       }
+                   }).max().unwrap();
+
+    println!("{max}");
 }
