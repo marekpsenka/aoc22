6ca78a885b5da4512528652a60694a7006f12548
Marek Pšenka <psenka@edhouse.cz>
2022-12-02 17:47:06 +0100

    Day 02

--- src/bin/day02b.rs
+++ src/bin/day02b.rs
@@ -1,5 +1,44 @@
-fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
+fn get_score(op_choice: char, my_choice: char) -> i32
+{
+    match op_choice {
+        'A' => {
+            match my_choice {
+                'X' => 0 + 3,
+                'Y' => 3 + 1,
+                'Z' => 6 + 2,
+                _ => panic!("My choice is invalid")
+
+            }
+        }
+        'B' => {
+            match my_choice {
+                'X' => 0 + 1,
+                'Y' => 3 + 2,
+                'Z' => 6 + 3,
+                _ => panic!("My choice is invalid")
+            }
+
+        }
+        'C' => {
+            match my_choice {
+                'X' => 0 + 2,
+                'Y' => 3 + 3,
+                'Z' => 6 + 1,
+                _ => panic!("My choice is invalid")
+
+            }
+        }
+        _ => panic!("Invalid opponent choice")
     }
+} 
+
+fn main() {
+    let mut my_score = 0;
+
+    for line in std::io::stdin().lines() {
+        let cs = line.unwrap().trim().chars().collect::<Vec<char>>();
+        my_score += get_score(cs[0], cs[2]);
+    }
+
+    println!("{my_score}");
 }
