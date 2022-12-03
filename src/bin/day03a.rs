728086e64a29a4d0d7f1c4a59b7efbe2129eaa47
Marek Pšenka <psenka@edhouse.cz>
2022-12-03 11:12:32 +0100

    Day03

--- src/bin/day03a.rs
+++ src/bin/day03a.rs
@@ -1,5 +1,27 @@
+use std::{collections::HashSet};
+
 fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
-    }
+    let result = std::io::stdin()
+        .lines()
+        .map(|maybe_line| {
+            let line = maybe_line.unwrap();
+            let left_compartment: HashSet<char> = line.chars().take(line.len() / 2).collect();
+            let right_compartment: HashSet<char> = line.chars().skip(line.len() / 2).collect();
+            let i: Vec<&char> = left_compartment.intersection(&right_compartment).collect();
+            if i.len() != 1 {
+                panic!("Only one type should be found in both compartments");
+            }
+            if (*i[0]).is_ascii_uppercase() {
+                (*i[0] as i32) - (64 - 26)
+            }
+            else if (*i[0]).is_ascii_lowercase()  {
+                (*i[0] as i32) - 96
+            }
+            else {
+                panic!("Unexpected character");
+            }
+        })
+        .sum::<i32>();
+
+    println!("{result}");
 }
--- src/bin/day03b.rs
+++ src/bin/day03b.rs
@@ -1,5 +1,45 @@
-fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
+use std::{collections::HashSet};
+
+use itertools::Itertools;
+
+fn get_priority(c: char) -> i32 {
+    if c.is_ascii_uppercase() {
+        (c as i32) - (64 - 26)
     }
+    else if c.is_ascii_lowercase()  {
+        (c as i32) - 96
+    }
+    else {
+        panic!("Unexpected character");
+    }
+}
+
+
+fn main() {
+    let result = std::io::stdin()
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(|chunk| {
+            let bags = chunk
+                .map(|maybe_line| {
+                    let line = maybe_line.unwrap();
+                    line.chars().collect::<HashSet<char>>()
+                })
+                .collect::<Vec<HashSet<char>>>();
+
+            if bags.len() != 3 {
+                panic!("Unexpected number of bags");
+            }
+            
+            let i1: HashSet<char> = bags[0].intersection(&bags[1]).into_iter().copied().collect();
+            let i2: Vec<&char> = i1.intersection(&bags[2]).collect();
+            if i2.len() != 1 {
+                panic!("Only one type should be found in all three compartments");
+            }
+            get_priority(*i2[0])
+        })
+        .sum::<i32>();
+
+    println!("{result}");
 }
