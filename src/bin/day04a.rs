73904d41220e6eca3bb4f628c478d900ecc86e9d
Marek Pšenka <psenka@edhouse.cz>
2022-12-04 10:42:00 +0100

    Day04

--- src/bin/day04a.rs
+++ src/bin/day04a.rs
@@ -1,5 +1,34 @@
+use itertools::Itertools;
+
+type Assignment = (i32, i32);
+
+fn parse_assignment(s: &str) -> Assignment {
+    let (sp1, sp2) = s.split('-').collect_tuple().unwrap();
+
+    (sp1.parse::<i32>().unwrap(), sp2.parse::<i32>().unwrap())
+}
+
+fn is_inside(some: &Assignment, other: &Assignment) -> bool {
+    let (l1, u1) = some;
+    let (l2, u2) = other;
+
+    l1 >= l2 && u1 <= u2
+}
+
 fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
-    }
+    let result = std::io::stdin()
+        .lines()
+        .map(|maybe_line| maybe_line.unwrap())
+        .filter(
+            |line| {
+                let (p1, p2) = line.split(',').collect_tuple().unwrap();
+                let a1 = parse_assignment(p1);
+                let a2 = parse_assignment(p2);
+
+                is_inside(&a1, &a2) || is_inside(&a2, &a1)
+            }
+        )
+        .count();
+
+    println!("{result}");
 }
--- src/bin/day04b.rs
+++ src/bin/day04b.rs
@@ -1,5 +1,34 @@
+use itertools::Itertools;
+
+type Assignment = (i32, i32);
+
+fn parse_assignment(s: &str) -> Assignment {
+    let (sp1, sp2) = s.split('-').collect_tuple().unwrap();
+
+    (sp1.parse::<i32>().unwrap(), sp2.parse::<i32>().unwrap())
+}
+
+fn do_overlap(some: &Assignment, other: &Assignment) -> bool {
+    let (l1, u1) = some;
+    let (l2, u2) = other;
+
+    u1 >= l2 && l1 <= u2
+}
+
 fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
-    }
+    let result = std::io::stdin()
+        .lines()
+        .map(|maybe_line| maybe_line.unwrap())
+        .filter(
+            |line| {
+                let (p1, p2) = line.split(',').collect_tuple().unwrap();
+                let a1 = parse_assignment(p1);
+                let a2 = parse_assignment(p2);
+
+                do_overlap(&a1, &a2)
+            }
+        )
+        .count();
+
+    println!("{result}");
 }
