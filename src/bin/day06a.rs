d14ba814d1c681c251e0d62818f4a9b73bf21070
Marek Pšenka <psenka@edhouse.cz>
2022-12-06 07:35:06 +0100

    Day 06

--- src/bin/day06a.rs
+++ src/bin/day06a.rs
@@ -1,5 +1,15 @@
+use std::collections::HashSet;
+
 fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+
+    let cs: Vec<char> = line.chars().collect();
+    for pos in 4..cs.len() {
+        let set: HashSet<char> = HashSet::from_iter(cs[(pos - 4)..pos].iter().cloned());
+        if set.len() == 4 {
+            println!("{pos}");
+            break;
+        }
     }
 }
--- src/bin/day06b.rs
+++ src/bin/day06b.rs
@@ -1,5 +1,15 @@
+use std::collections::HashSet;
+
 fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
+    let mut line = String::new();
+    std::io::stdin().read_line(&mut line).unwrap();
+
+    let cs: Vec<char> = line.chars().collect();
+    for pos in 14..cs.len() {
+        let set: HashSet<char> = HashSet::from_iter(cs[(pos - 14)..pos].iter().cloned());
+        if set.len() == 14 {
+            println!("{pos}");
+            break;
+        }
     }
 }
