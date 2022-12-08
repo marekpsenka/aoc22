538358289755d65d948c5db11a197a3234180e5e
Marek Pšenka <psenka@edhouse.cz>
2022-12-08 10:24:02 +0100

    Day08

--- src/bin/day08a.rs
+++ src/bin/day08a.rs
@@ -1,5 +1,68 @@
-fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
+#[derive(Debug)]
+struct Tree {
+    height: i32,
+    is_visible: bool,
+}
+
+type Map = Vec::<Vec<Tree>>;
+
+fn mark_visible_if_taller(i: usize, j: usize, tallest: &mut i32, map: &mut Map) {
+    let this_tree = &mut map[i][j];
+    if this_tree.height > *tallest {
+        *tallest = this_tree.height;
+        this_tree.is_visible = true;
     }
+}
+
+fn main() {
+    let mut map = Map::new();
+
+    std::io::stdin()
+        .lines()
+        .map(|maybe_line| maybe_line.expect("Successful read"))
+        .for_each(|line| {
+            let row = line
+                .chars()
+                .map(|c| Tree {
+                    height: c
+                        .to_digit(10).expect("parsing a digit")
+                        .try_into().expect("digit can be converted to i32"),
+                    is_visible: false,
+                })
+                .collect::<Vec<Tree>>();
+
+            map.push(row);
+        });
+
+    let width = map.first().expect("At least one row").len();
+    let height = map.len();
+
+    for j in 0..width {
+        let mut tallest = i32::MIN;
+        for i in 0..height {
+            mark_visible_if_taller(i, j, &mut tallest, &mut map);
+        } 
+        tallest = i32::MIN;
+        for i in (0..height).rev() {
+            mark_visible_if_taller(i, j, &mut tallest, &mut map);
+        } 
+    }
+
+    for i in 0..height {
+        let mut tallest = i32::MIN;
+        for j in 0..width {
+            mark_visible_if_taller(i, j, &mut tallest, &mut map);
+        } 
+        tallest = i32::MIN;
+        for j in (0..width).rev() {
+            mark_visible_if_taller(i, j, &mut tallest, &mut map);
+        } 
+    }
+
+    let visible_count = map
+        .iter()
+        .map(|row| row.iter().filter(|t| t.is_visible).count())
+        .sum::<usize>();
+
+    println!("{visible_count}");
 }
--- src/bin/day08b.rs
+++ src/bin/day08b.rs
@@ -1,5 +1,88 @@
+#[derive(Debug)]
+struct Tree {
+    height: i32,
+}
+
+type Map = Vec::<Vec<Tree>>;
+
+fn get_scenic_score(width: usize, height: usize, i: usize, j: usize, map: &mut Map) -> usize {
+    let origin_height = map[i][j].height;
+    let mut score = 1usize;
+
+    let mut direction_score = 0usize;
+    for ii in (0..i).rev() {
+        direction_score += 1;
+        if map[ii][j].height >= origin_height {
+            break;
+        }
+    } 
+
+    score *= direction_score;
+    direction_score = 0;
+
+    for ii in (i + 1)..height {
+        direction_score += 1;
+        if map[ii][j].height >= origin_height {
+            break;
+        }
+    } 
+
+    score *= direction_score;
+    direction_score = 0;
+
+    for jj in (0..j).rev() {
+        direction_score += 1;
+        if map[i][jj].height >= origin_height {
+            break;
+        }
+    } 
+
+    score *= direction_score;
+    direction_score = 0;
+
+    for jj in (j + 1)..width {
+        direction_score += 1;
+        if map[i][jj].height >= origin_height {
+            break;
+        }
+    } 
+    score *= direction_score;
+    score
+}
+
 fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
+    let mut map = Map::new();
+
+    std::io::stdin()
+        .lines()
+        .map(|maybe_line| maybe_line.expect("Successful read"))
+        .for_each(|line| {
+            let row = line
+                .chars()
+                .map(|c| Tree {
+                    height: c
+                        .to_digit(10).expect("parsing a digit")
+                        .try_into().expect("digit can be converted to i32"),
+                })
+                .collect::<Vec<Tree>>();
+
+            map.push(row);
+        });
+
+    let width = map.first().expect("At least one row").len();
+    let height = map.len();
+
+    let mut highest_scenic_score = usize::MIN;
+
+    println!("{}", get_scenic_score(width, height, 3, 2, &mut map));
+
+    for i in 0..height {
+        for j in 0..width {
+            let score = get_scenic_score(width, height, i, j, &mut map);
+            if score > highest_scenic_score {
+                highest_scenic_score = score;
+            }
+        }
     }
+    println!("{highest_scenic_score}");
 }
