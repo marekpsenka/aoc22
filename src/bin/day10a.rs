e7cc3ec4b6ef2575acc8ece95e8ac6e04e0b2d5e
Marek Pšenka <psenka@edhouse.cz>
2022-12-11 09:16:31 +0100

    Day10

--- src/bin/day10a.rs
+++ src/bin/day10a.rs
@@ -1,5 +1,33 @@
+use itertools::Itertools;
+
 fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
+    let mut x = 1i32;
+    let mut cycle_values = Vec::<i32>::new();
+
+    for maybe_line in std::io::stdin().lines() {
+        let line = maybe_line.expect("Successful read");
+        if line.starts_with("noop") {
+            cycle_values.push(x);
+        }
+        else {
+            let (_, value_str) = line
+                .split_ascii_whitespace()
+                .collect_tuple()
+                .expect("Pair");
+
+            let value = value_str.parse::<i32>().expect("Successful parse");
+            cycle_values.push(x);
+            cycle_values.push(x);
+            x += value;
+        }
     }
+
+    let result = vec![20, 60, 100, 140, 180, 220]
+        .iter()
+        .map(|&i| {
+            i * cycle_values[i - 1] as usize
+        })
+        .sum::<usize>();
+
+    println!("{result}");
 }
--- src/bin/day10b.rs
+++ src/bin/day10b.rs
@@ -1,5 +1,47 @@
+use itertools::Itertools;
+
 fn main() {
-    for line in std::io::stdin().lines() {
-        println!("{}", line.unwrap());
+    let mut x = 1i32;
+    let mut cycle_values = Vec::<i32>::new();
+
+    for maybe_line in std::io::stdin().lines() {
+        let line = maybe_line.expect("Successful read");
+        if line.starts_with("noop") {
+            cycle_values.push(x);
+        }
+        else {
+            let (_, value_str) = line
+                .split_ascii_whitespace()
+                .collect_tuple()
+                .expect("Pair");
+
+            let value = value_str.parse::<i32>().expect("Successful parse");
+            cycle_values.push(x);
+            cycle_values.push(x);
+            x += value;
+        }
+    }
+
+    let width = 40;
+    let height = 6;
+
+    let mut pixels = cycle_values
+        .iter()
+        .enumerate()
+        .map(|(i, &x)| {
+            if (i % width).abs_diff(x as usize) <= 1 {
+                '#'
+            }
+            else {
+                '.'
+            }
+        });
+
+    for _ in 0..height {
+        for _ in 0..width {
+            print!("{}", pixels.next().expect("Value is yielded"));
+        }
+        println!();
     }
 }
+
